8bd92fd14972d8b3d41ac8a309b2d04e631c91e7
hegenbarth <andreas.hegenbarth@beyerdynamic.de>
2023-07-07 20:48:57 +0200

    add comment moderate

--- comments/index.js
+++ comments/index.js
@@ -15,13 +15,19 @@ app.get("/posts/:id/comments", (req, res) => {
 });
 
 app.post("/posts/:id/comments", async (req, res) => {
+  // generate Radom ID
   const commentId = randomBytes(4).toString("hex");
+
+  // get the content from the request body
   const { content } = req.body;
 
+  // get the comments array for the post id
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  // push the new comment to the comments array
+  comments.push({ id: commentId, content, status: "pending" });
 
+  // update the comments array for the post id
   commentsByPostId[req.params.id] = comments;
 
   await axios.post("http://localhost:4005/events", {
@@ -30,14 +36,37 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Event Received", req.body.type);
+
+  const { type, data } = req.body;
+
+  if( type === 'CommentModerated' ) {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+     
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    })
+  }
 
   res.send({});
 });
